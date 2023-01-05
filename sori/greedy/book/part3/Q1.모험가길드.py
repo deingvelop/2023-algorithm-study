364c37d3f4326a55093fe4ca9854ee15ce436be6
imsori <dlathfl868@naver.com>
2023-01-05 17:00:36 +0900

    [TCT-P3] 모험가 길드

--- sori/greedy/book/part3/Q1.모험가길드.py
+++ sori/greedy/book/part3/Q1.모험가길드.py
@@ -1,1 +1,19 @@
-#문제풀이
+#모험가 n명
+#공포도 x인 모험가는 그룹인원이 x이상인 그룹에만 참여가능
+#몇명은 남아도됨.
+#그룹수의 최댓값 구하기
+#최댓값중의 최솟값으로 묶기..
+
+n = map(int, input().split())
+data = list(map(int, input().split()))
+
+data = data.sort()
+result = 0
+index = 0
+for i in data.length():
+  index += 1
+  if data[i] >= index:
+    result += 1
+    index = 0
+
+print(result)
