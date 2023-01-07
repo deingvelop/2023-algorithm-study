8ed84e94168473e1daee612e002bb12a75cbfddf
minjudy <56119301+minjudy@users.noreply.github.com>
2023-01-07 15:38:58 +0900

    Update Q1.큰수의법칙.py

--- minju/greedy/book/part2/Q1.큰수의법칙.py
+++ minju/greedy/book/part2/Q1.큰수의법칙.py
@@ -1,9 +1,11 @@
+"""순서 상관 없이 큰 수 2개만 사용해서 조합"""
+
 n,m,k = map(int, input().split())
 data = list(map(int, input().split()))
 
 result = 0 
 
-data.sort(reverse=True)
+data.sort(reverse=True) #내림차순 정렬
 
 result = ((m//k)*k*data[0]) + ((m%k)*data[1])
 print(result)
