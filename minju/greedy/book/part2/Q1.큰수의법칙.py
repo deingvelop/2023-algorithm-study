0ddccf4c26eb4c4497d70bba678f6e9dd6e1cf35
minjudy <56119301+minjudy@users.noreply.github.com>
2023-01-07 15:40:46 +0900

    [TCT-P2] 큰 수의 법칙 / 20분

--- minju/greedy/book/part2/Q1.큰수의법칙.py
+++ minju/greedy/book/part2/Q1.큰수의법칙.py
@@ -7,5 +7,5 @@ result = 0
 
 data.sort(reverse=True) #내림차순 정렬
 
-result = ((m//k)*k*data[0]) + ((m%k)*data[1])
+result = ((m//k)*k*data[0]) + ((m%k)*data[1]) #가장큰 수를 최대한 사용하고, 그 다음 큰수를 나머지만큼 사용
 print(result)
