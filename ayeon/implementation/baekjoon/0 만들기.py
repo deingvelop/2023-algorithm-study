f5679ab4482d73066f5cce94735ced606716f902
Ayeon Kim <77188666+yourzinc@users.noreply.github.com>
2023-01-22 17:27:20 +0900

    [BOJ] 0 만들기 / 72ms / 골드5

---
+++ ayeon/implementation/baekjoon/0 만들기.py
@@ -0,0 +1,54 @@
+# + ,
+# - ,
+# 숫자 이어붙이기
+
+# N이 주어졌을 때 수식의 결과가 0이 되는 모든 수식을 찾는 프로그램
+
+N = int(input())
+
+def check(result):
+    a = 0
+    result = result.replace(" ", "")
+    n = ''
+    for i in range(len(result)-1, -1, -1):
+        if result[i] == '+' :
+            a += int(n)
+            n = ''
+        elif result[i] == '-':
+            a -= int(n)
+            n = ''
+        else:
+            n = result[i] + n
+    
+    if n != '':
+        a += int(n)
+        
+    if a == 0:
+        return True
+    return False
+    
+
+def Recursive(step, arr, result, answer):
+    if step == len(arr)-1:
+        result = result+str(arr[step])
+        if check(result):
+            answer.append(result)
+        return
+
+    Recursive(step+1, arr, result+str(arr[step])+' ', answer)
+    Recursive(step+1, arr, result+str(arr[step])+'+', answer)
+    Recursive(step+1, arr, result+str(arr[step])+'-', answer)
+ 
+answer = []  
+
+for i in range(N):
+    M = int(input())
+    arr = [ i+1 for i in range(M) ]
+    result = ""
+    
+    Recursive(0, arr, result, answer)
+    answer.append("")
+
+answer.pop()
+for a in answer:
+    print(a)
