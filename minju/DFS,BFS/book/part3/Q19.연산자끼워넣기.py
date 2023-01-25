168c6de2ce6abd2221ef8761d8640df0f6cb50be
minjudy <56119301+minjudy@users.noreply.github.com>
2023-01-25 12:20:32 +0900

    [TCT-P3] 연산자 끼워넣기

--- minju/DFS,BFS/book/part3/Q19.연산자끼워넣기.py
+++ minju/DFS,BFS/book/part3/Q19.연산자끼워넣기.py
@@ -1,34 +1,25 @@
 n = int(input())
-num = list(map(int, input().split()))
-op = list(map(int, input().split()))  # +, -, *, //
+data = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
+max_value = -int(1e9)
+min_value = int(1e9)
 
-max_value= -1e9
-min_value= 1e9
+def dfs(depth, num, add, sub, mul, div):
+    global max_value, min_value
+    if depth == n:
+        max_value = max(max_value, num)
+        min_value = min(min_value, num)
+    else:
+        if add:
+            dfs(depth+1, num+data[depth], add-1, sub, mul, div)
+        if sub:
+            dfs(depth+1, num-data[depth], add, sub-1, mul, div)
+        if mul:
+            dfs(depth+1, num*data[depth], add, sub, mul-1, div)
+        if div:
+            dfs(depth+1, int(num/data[depth]), add, sub, mul, div-1)
 
-def dfs(i,arr):
-  global num, op, max_value, min_value
+dfs(1, data[0], add, sub, mul, div)
 
-  if i == n:
-    max_value = max(max_value, arr)
-    min_value = min(min_value, arr)
-  else:
-    if op[0]>0:
-      op[0] -= 1
-      dfs(i+1, arr + num[i])
-      op[0] += 1
-    if op[1]>0:
-      op[1] -= 1
-      dfs(i+1, arr - num[i])
-      op[1] += 1
-    if op[2]>0:
-      op[2] -= 1
-      dfs(i+1, arr * num[i])
-      op[2] += 1
-    if op[3]>0:
-      op[3] -= 1
-      dfs(i+1, arr / num[i])
-      op[3] += 1  
-      
-dfs(1,num[0])      
 print(max_value)
 print(min_value)
