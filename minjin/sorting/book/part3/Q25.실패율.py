38405170f5f4ee6f87b6b944eb0e88a498f793ad
송민진 <m_6595@naver.com>
2023-01-30 20:36:20 +0900

    [TCT-P3] 실패율 / 난이도1 https://school.programmers.co.kr/learn/courses/30/lessons/42889

---
+++ minjin/sorting/book/part3/Q25.실패율.py
@@ -0,0 +1,55 @@
+# 나의 풀이 - 테스트케이스 12번만 오류
+def solution(N, stages):
+    failures = [0 for _ in range(N+1)]
+    reaches = [0 for _ in range(N + 1)]
+    fail_rates = [[i, 0] for i in range(N+1)]
+
+    for stage in stages:
+        if stage == N+1:
+            reaches[N] += 1
+            for s in range(1, stage):
+                reaches[s] += 1
+        else:
+            failures[stage] += 1
+            for s in range(1, stage+1):
+                reaches[s] += 1
+
+    for i in range(1, N+1):
+        if reaches[i] == 0:
+            fail_rates[i][1] = 0
+        else:
+            fail_rates[i][1] = failures[i] / reaches[i]
+    fail_rates.sort(key=lambda x:(-x[1], x[0]))
+
+    answer = []
+    for f in fail_rates:
+        if f[0] != 0:
+            answer.append(f[0])
+    return answer
+
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(10, [1, 2, 3, 3, 4, 5, 5]))
+print(solution(5,[2, 2, 2, 2, 2, 2, 2, 2]))
+print(solution(1,[1]))
+print(solution(1,[2]))
+
+
+# 교재 풀이
+def solution(N, stages):
+    answer = []
+    length = len(stages)
+
+    for i in range(1, N+1):
+        count = stages.count(i)
+
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+
+        answer.append((i, fail))
+        length -= count
+
+    answer = sorted(answer, key=lambda x:-x[1])
+    answer = [i[0] for i in answer]
+    return answer
