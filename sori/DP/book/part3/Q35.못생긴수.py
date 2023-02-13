a96e81359e4b574f2ac7409c1039bb5c4ef42f05
imsori <dlathfl868@naver.com>
2023-02-13 17:02:10 +0900

    [TCT-3] Q32.정수 삼각형 / 30분

--- sori/DP/book/part3/Q35.못생긴수.py
+++ sori/DP/book/part3/Q35.못생긴수.py
@@ -5,7 +5,8 @@ n = int(input())
 ugly = [0] * n
 ugly[0] = 1 #첫번째 못생긴 수는 1
 
-#못생기수에 2,3,5 곱한 수 또한 못생기 수이다.
+# * 못생긴수에 2,3,5 곱한 수 또한 못생긴 수이다.
+# 바텀업 방식으로 1에 못생긴수 곱해가며 소인수 배열 만들기
 
 #2배, 3배, 5배 인덱스
 i2 = i3 = i5 = 0
@@ -17,21 +18,16 @@ for i in range(1, n):
   ugly[i] = min(next2, next3, next5)
                #1st : 2, 3, 5
                #2nd : 4, 3, 5
- # print("i:", i,",ugly[",i,"] :" ,ugly[i])
   if ugly[i] == next2:
     i2 += 1
     next2 = ugly[i2] * 2
-    #print("i2 :", i2)
-    #1st : ugly[1] *2 = 2 * 2
   if ugly[i] == next3:
     i3 += 1
     next3 = ugly[i3] * 3
-    #print("i3 :", i3)
-    #2nd : ugly[1] * 3 = 2 * 3 = 6
   if ugly[i] == next5:
     i5 += 1
     next5 = ugly[i5] * 5
-    #print("i5 :", i5)
+  #elif 로 하면 안됨. 공통 인수가 있기 때문에
 
 print(ugly[n-1])
   
