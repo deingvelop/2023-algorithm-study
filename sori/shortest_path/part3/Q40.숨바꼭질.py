339cdfd5aaf0f982ac62e4b4e5a26aed33670c46
imsori <dlathfl868@naver.com>
2023-02-20 14:43:28 +0900

    [TCT-3] Q40.숨바꼭질 수정

--- sori/shortest_path/part3/Q40.숨바꼭질.py
+++ sori/shortest_path/part3/Q40.숨바꼭질.py
@@ -9,20 +9,23 @@ for _ in range(m):
   graph[b].append((a,1))
 
 INF = int(1e9)
-start = 1
 distance = [INF] * (n+1)
 
 def dijkstra(start):
   #시작 노드
   distance[start] = 0
   q = []
+  #시작 노드로 가는 최단경로는 0으로 설정
   heapq.heappush(q, (0,start))
   while q:
     dist, node = heapq.heappop(q)
+    #이미 처리되어 있으면 무시
     if dist > distance[node]:
       continue
+    #현재 노드와 연결된 다른 인접 노드들을 확인
     for idx, dis in graph[node]:
       cost = distance[node] + dis
+      #현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우 업데이트하기
       if cost < distance[idx]:
         distance[idx] = cost
         heapq.heappush(q,(cost, idx))
@@ -38,4 +41,7 @@ def dijkstra(start):
   for i in range(1, n+1):
     if distance[i] == max_dist:
       node.append(i)
+      #숨어야하는 헛간번호(가장번호작은), 거리, 같은 거리 헛간갯수
   print(node[0], max_dist, len(node))
+
+dijkstra(1)
